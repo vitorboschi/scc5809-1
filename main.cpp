bb8d4b2d8df9dec9bdc58a06acc8c2c5aecc18dc
Felipe Gomes <felipc@gmail.com>
2015-08-31 16:01:04 -0300

    Primeira rotina para ler amostras a partir de um arquivo de texto

--- main.cpp
+++ main.cpp
@@ -1,14 +1,69 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
 #include "trainingData.h"
 #include "perceptron.h"
 
 const int maxTrainingCycles = 100;
 const float delta = 0.01;
 
+#define WIDTH 8
+#define HEIGHT 8
+
+int readSamples(const char * fileName, TrainingSample* samples, int maxSamples) {
+	int nSamples = 0;
+	std::ifstream file(fileName);
+
+	std::string line;
+	int result;
+
+	while (nSamples < maxSamples) {
+		if (!std::getline(file, line)) {
+			break;
+		}
+
+		if (line.compare("") == 0) {
+			// empty line
+			continue;
+		}
+
+		result = stoi(line);
+		printf("result is %d\n", result);
+
+		for (int i = 0; i < HEIGHT; i++) {
+			if (!std::getline(file, line) ||
+				line.size() < WIDTH) {
+				std::cout << "Input #" << (nSamples + 1) << " is malformed on line " << (i + 1) << "." << std::endl;
+				exit(0);
+			}
+			
+			for (int j = 0; j < WIDTH; j++) {
+				samples[nSamples].input[i * WIDTH + j] = 
+				  line[j] == '*' ? 1 : 0;
+			}
+		}
+
+		nSamples++;
+	}
+
+	return nSamples;
+}
+
+
+
+
 int main(int argc, char** argv) {
 	Perceptron p(64);
 	int currentCycle = 0;
 	int cycleError = 100;
+
+	TrainingSample k[100];
+
+	printf("\n\nnum Samples: %d\n\n", readSamples("trainingset.txt", k, 10));
 
 	while (cycleError > 0 && currentCycle < maxTrainingCycles) {
 		std::cout << "Starting training cycle " << currentCycle << std::endl;
@@ -25,3 +80,5 @@ int main(int argc, char** argv) {
 	std::cout << std::endl << "Traning concluded with " << currentCycle << " cycles" << std::endl;
 	return 0;
 }
+
+
