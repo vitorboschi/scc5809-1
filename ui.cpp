0fc2408b1b7c5d6baccf1acf8e873f224f043fbf
Vitor Boschi <vitorboschi@gmail.com>
2015-09-02 23:30:45 -0300

    find and draw bounding rect

--- ui.cpp
+++ ui.cpp
@@ -2,6 +2,7 @@
 
 #include <opencv2/opencv.hpp>
 #include <iostream>
+#include <vector>
 
 static void onMouse( int event, int x, int y, int, void* parameter) {
 	static bool isMouseDown = false;
@@ -11,13 +12,26 @@ static void onMouse( int event, int x, int y, int, void* parameter) {
 
 	if (event == cv::EVENT_LBUTTONDOWN) {
 		std::cout << "Started drawing" << std::endl;
-		*canvas = cv::Scalar(0,0,0);
+		*canvas = cv::Scalar(0);
 		lastX = x;
 		lastY = y;
 		isMouseDown = true;
 	}
 
 	if (event == cv::EVENT_LBUTTONUP) {
+		std::vector<std::vector <cv::Point> > contours;
+
+		findContours(*canvas, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE, cv::Point(0, 0));
+
+ 		std::vector<cv::Rect> boundRect( contours.size() );
+		
+		for( int i = 0; i < contours.size(); i++ ) {
+			boundRect[i] = cv::boundingRect(contours[i]);
+			cv::rectangle(*canvas, boundRect[i].tl(), boundRect[i].br(), cv::Scalar(255), 1, 8, 0 );
+
+		}
+
+		imshow("canvas", *canvas);
 		std::cout << "End drawing" << std::endl;
 		isMouseDown = false;
 
